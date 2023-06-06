7b0197b24d7e51685fe5c1efb686d2a32c92018b
Skip Hansen <skip@gfrn.org>
2023-06-06 16:11:04 -0700

    Initial commit - blinky runs.

---
+++ fw/include/pano_io.h
@@ -0,0 +1,20 @@
+#ifndef _PANIO_IO_H_
+#define _PANIO_IO_H_
+
+#define GPIO_BASE             0x94000000
+#define ETH_BASE              0x95000000
+
+#define GPIO_BIT_PANO_BUTTON  0x02
+#define GPIO_BIT_RED_LED      0x04
+#define GPIO_BIT_GREEN_LED    0x08
+#define GPIO_BIT_BLUE_LED     0x10
+#define GPIO_BIT_CODEC_SDA    0x20
+#define GPIO_BIT_CODEC_SCL    0x40
+
+#define GPIO_LED_BITS         (GPIO_BIT_RED_LED  | \
+                              GPIO_BIT_GREEN_LED | \
+                              GPIO_BIT_BLUE_LED)
+
+
+#endif   // _PANIO_IO_H_
+
